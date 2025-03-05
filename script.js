31497eacbe7d17482e27205671283a63397d7503
thaha-official <thaharasul59@gmail.com>
2025-03-05 10:45:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/* script.js */
+// script.js
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch (e) {
+        alert("Invalid Expression");
+    }
+}
